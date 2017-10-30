84c38f3686ad0b78929dc2c71ffcb1ec5d96a08f
Maksym Glubokiy <M.Glubokiy@gmail.com>
2017-10-30 23:27:09 +0200

    beagle: add beagle_deploy() to beagle env script

--- scripts/beagle_env.sh
+++ scripts/beagle_env.sh
@@ -8,3 +8,23 @@ export BUILD_ROOT_OVERLAY="${BASE_DIR}/rootfs_extras/overlay/"
 export BUILD_ROOT_USERS="${BASE_DIR}/rootfs_extras/users"
 export ARCH="arm"
 export CROSS_COMPILE="arm-linux-gnueabihf-"
+
+export INSTALL_BASE="${BASE_DIR}/beagle_deploy/"
+
+unset beagle_deploy
+
+beagle_deploy() {
+	local boot_path="${INSTALL_BASE}/boot/"
+	local mod_path="${INSTALL_BASE}"
+	local kernel_version=$(make kernelversion)
+	local modules_dir="${mod_path}/lib/modules/${kernel_version}/"
+
+	rm -rf "${boot_path}" "$mod_path"
+
+	mkdir -p "${boot_path}"
+	mkdir -p "${mod_path}"
+
+	INSTALL_PATH="${boot_path}" INSTALL_MOD_PATH="${mod_path}" make zinstall dtbs_install modules_install
+
+	rm -f "${modules_dir}/source" "${modules_dir}/build"
+}
