d7d3681cc866b49f8dc194b2b8fd6b5e8fadb8ba
Maksym Glubokiy <M.Glubokiy@gmail.com>
2017-10-31 00:12:41 +0200

    scripts: x86: unset CROSS_COMPILE

--- scripts/x86_env.sh
+++ scripts/x86_env.sh
@@ -7,3 +7,4 @@ export BUILD_ROOT="${BASE_DIR}/rootfs/"
 export BUILD_ROOT_OVERLAY="${BASE_DIR}/rootfs_extras/overlay/"
 export BUILD_ROOT_USERS="${BASE_DIR}/rootfs_extras/users"
 export ARCH="i386"
+unset CROSS_COMPILE
